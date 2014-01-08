a0f6bddd52bc8dd820fdc34021ce355931ae0af6
gchopelet@gmail.com <gchopelet@gmail.com@facc5fb7-2ffd-1558-d52c-9919e05b8af2>
2014-01-08 16:34:41 +0000

    Initial commit

---
+++ trunk/Elinor/Profiles.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Elinor
+{
+    public class Profiles
+    {
+        private static readonly DirectoryInfo _profdir = new DirectoryInfo("profiles");
+
+        public static List<Profile> getAllProfiles() 
+        {
+            List<Profile> profiles = new List<Profile>();
+
+            foreach (FileInfo file in _profdir.GetFiles())
+            {
+                Profile profile = Profile.ReadSettings(file.Name.Replace(file.Extension, ""));
+
+                if (profile != null)
+                {
+                    profiles.Add(profile);
+                }
+            }
+
+            return profiles;
+        }
+
+    }
+}
