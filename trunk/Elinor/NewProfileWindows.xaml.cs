9492fea7ec65437eb62304a2c971d37a4896059f
gchopelet@gmail.com <gchopelet@gmail.com@facc5fb7-2ffd-1558-d52c-9919e05b8af2>
2014-03-18 15:29:14 +0000

    Merge new and import button

---
+++ trunk/Elinor/NewProfileWindows.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Elinor
+{
+    /// <summary>
+    /// Logique d'interaction pour NewProfileWindow.xaml
+    /// </summary>
+    public partial class NewProfileWindow : Window
+    {
+
+        protected MainWindow mainWindow;
+
+        public NewProfileWindow(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+        }
+
+        private void WindowKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape) Close();
+        }
+
+        private void btnCreateNew_Click(object sender, RoutedEventArgs e)
+        {
+            var profileNameWindow = new ProfileNameWindow(mainWindow)
+            {
+                Topmost = true,
+                Top = Top,
+                Left = Left,
+            };
+
+            profileNameWindow.ShowDialog();
+
+            if (profileNameWindow.DialogResult == true)
+            {
+                Close();
+            }
+        }
+
+        private void btnImportAPI_Click(object sender, RoutedEventArgs e)
+        {
+            var aiw = new ApiImportWindow()
+            {
+                Topmost = true,
+                Top = Top,
+                Left = Left,
+            };
+
+            if (aiw.ShowDialog() == true)
+            {
+                Profile settings = aiw.profile;
+                string fName = string.Format("profiles\\{0}.dat", settings.profileName);
+
+                if (File.Exists(fName))
+                {
+                    MessageBoxResult result = MessageBox.Show(
+                        "Character exists. Update?",
+                        "Character already exists",
+                        MessageBoxButton.YesNo,
+                        MessageBoxImage.Information
+                    );
+
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        int index = 1;
+                        File.Delete(fName);
+                        for (int i = 0; i < mainWindow.cbProfiles.Items.Count; i++)
+                        {
+                            var tmp = (Profile)mainWindow.cbProfiles.Items[i];
+                            if (tmp.profileName == settings.profileName)
+                            {
+                                index = i;
+                                mainWindow.cbProfiles.SelectedIndex = 0;
+                                mainWindow.cbProfiles.Items.RemoveAt(i);
+                                break;
+                            }
+                        }
+                        mainWindow.cbProfiles.Items.Insert(index, settings);
+                        mainWindow.cbProfiles.SelectedItem = settings;
+                        mainWindow.tcMain.SelectedIndex = 1;
+                    }
+                }
+                else
+                {
+                    mainWindow.cbProfiles.Items.Add(settings);
+                    mainWindow.cbProfiles.SelectedItem = settings;
+                    mainWindow.tcMain.SelectedIndex = 1;
+                }
+
+                Close();
+            }            
+        }
+    }
+}
