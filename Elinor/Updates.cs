b5110d22ab9854f8b46a0d5e32cef86624e13b09
Slivo-fr <gchopelet@gmail.com>
2021-11-11 15:50:06 +0100

    remove debug logs

--- Elinor/Updates.cs
+++ Elinor/Updates.cs
@@ -54,9 +54,6 @@ namespace Elinor
                         var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
                         var currentVersion = new Version(fvi.ProductVersion);
 
-                        Console.WriteLine(currentVersion);
-                        Console.WriteLine(newVersion);
-
                         if (newVersion > currentVersion)
                         {
                             // TODO: Manage new version check
